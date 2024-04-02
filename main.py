329e0dbab611d27d501beda51665720d37d29145
krukarina <68153911+krukarina@users.noreply.github.com>
2024-04-02 23:58:12 +0100

    Worked on GUI

--- main.py
+++ main.py
@@ -1,45 +1,57 @@
 import kivy
+
 kivy.require('2.3.0')
+
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.config import Config
 from kivy.core.window import Window
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
-from kivy.uix.button import Button
-from kivy.uix.textinput import TextInput
 from kivy.uix.screenmanager import ScreenManager, Screen
 
-#Set configuration
+# Set configuration
 Config.set('graphics', 'width', '720dp')
 Config.set('graphics', 'height', '1080dp')
 
 Window.clearcolor = "#f2f1ec"
 
-#Define different screens
+
+# Define different screens
 class View(Screen):
     pass
+
 
 class GreetPage(Screen):
     pass
 
+
 class LoginPage(Screen):
     pass
 
+
 class RegisterPage(Screen):
+    pass
+
+class RegfinishPage(Screen):
     pass
 
 class MainPage(Screen):
     pass
 
+
 class AddPage(Screen):
     pass
+
 
 class ShowPage(Screen):
     pass
 
+
 class EditPage(Screen):
     pass
+
+class SearchPage(Screen):
+    pass
+
 
 class WindowManager(ScreenManager):
     pass
@@ -48,6 +60,7 @@ class CriptiqueApp(App):
 
     def build(self):
         return kv
+
 
 kv = Builder.load_file('criptique.kv')
 
