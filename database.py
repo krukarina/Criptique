b826776bde3df421568ecf16be1e5cf74611493c
krukarina <68153911+krukarina@users.noreply.github.com>
2024-04-07 18:22:42 +0100

    Worked on GUI

--- database.py
+++ database.py
@@ -1,36 +1,6 @@
-import psycopg2
+import sqlite3
 
-hostname = "localhost"
-database = "postgres"
-username = "postgres"
-password = "7E6giDiK7o"
-port_id = 5432
-conn = None
-cur = None
-try:
-    conn = psycopg2.connect(
-        host=hostname,
-        dbname=database,
-        user=username,
-        password=password,
-        port=port_id)
+conn = sqlite3.connect('criptique_login.db')
 
-    cur = conn.cursor()
+cur = conn.cursor()
 
-    cur.execute("""CREATE TABLE IF NOT EXISTS criptiquedata (
-    id INT PRIMARY KEY,
-    socialmedianame VARCHAR(255),
-    login VARCHAR(255),
-    password VARCHAR(255)
-    )
-    """)
-    conn.commit()
-
-
-except Exception as error:
-    print(error)
-finally:
-    if cur is not None:
-        cur.close()
-    if conn is not None:
-        conn.close()
