9294d9cb217888ce217032e460794fa52871618f
krukarina <68153911+krukarina@users.noreply.github.com>
2024-04-16 02:31:01 +0100

    final version of an app

--- hashing.py
+++ hashing.py
@@ -5,13 +5,3 @@ def hash_masterpwd(password):
     salt = bcrypt.gensalt()
     hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed.decode('utf-8'), salt
-
-# def checkpwd(password, hashed_password, email):
-#   salt = get_salt(email)
-#  if not salt:
-#     print('User not found.')
-#    return False
-# else:
-#   hash_input = bcrypt.hashpw(password.encode('utf-8'), salt.encode('utf-8'))
-#  check = bcrypt.checkpw(hash_input, hashed_password)
-# return check
